785f790f72e1773c12fea65c2a77fed44d237b42
Kashishduhan <132093256+Kashishduhan@users.noreply.github.com>
2023-05-23 11:51:21 +0530

    Create Vector.cpp

---
+++ Practice_code/Vector.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{ 
+    int temp;
+    vector<int>vec;
+    for(int i=0,i<10,i++)
+    {
+     cin>>temp;
+     vec.push_back(temp);
+    }
+    for(int i=0,i<10;i++){
+     cout<<vec[i]<<" ";
+    }
+    return 0;
+}
+ 
